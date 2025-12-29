3244c53daa40d6eb201185244fbca393ce71d7cf
Aditya Raj Singh Ranawat <adityarajsinghranawatjobs@gmail.com>
2025-12-29 19:48:26 +0530

    refactor formatting and comments in function return annotations example in script.ts

--- 09. Function Return Annotations/script.ts
+++ 09. Function Return Annotations/script.ts
@@ -14,24 +14,19 @@
 */
 
 // BUT, this will give error:
-function double(x: number, y: boolean) : number {
-    // Error:
-    // return y;    // warning
-    return x;
+function double(x: number, y: boolean): number {
+  // Error:
+  // return y;    // warning
+  return x;
 }
 console.log(double(2, true));
-
 
 // ARROW FUNCTIOON:
 
 const checker = (x: number, y: boolean): number => x;
 
 // Error:
-// const checker = (x: number, y: boolean): number => y; 
+// const checker = (x: number, y: boolean): number => y;
 
 const res = checker(2, true);
 console.log(res);
-
-
-
-
