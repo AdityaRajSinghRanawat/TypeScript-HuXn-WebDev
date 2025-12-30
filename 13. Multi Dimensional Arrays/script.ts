e1f1056e4c68293337093ccd7fbec6590a6bb0e3
Aditya Raj Singh Ranawat <adityarajsinghranawatjobs@gmail.com>
2025-12-30 18:02:00 +0530

    add multi-dimensional array examples and documentation in script.ts

---
+++ 13. Multi Dimensional Arrays/script.ts
@@ -0,0 +1,18 @@
+/*
+    Multi Dimensional:
+    
+    A multi-dimensional array is an array that contains 
+    other arrays as its elements. 
+    
+    Multi-dimensional arrays can be defined using the 
+    same notation as one-dimensional arrays, 
+    but with nested square brackets.
+*/
+
+const arr1D: number[] = [2, 5, 6];
+const arr2D: number[][] = [[2, 5, 6]];
+const arr3D: number[][][] = [[[2, 5, 6]]];
+
+console.log(arr1D);
+console.log(arr2D);
+console.log(arr3D);
