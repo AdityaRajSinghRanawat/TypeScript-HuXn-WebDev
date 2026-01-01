c13c33f972e5694985eafb859d9bd237e540c5c0
Aditya Raj Singh Ranawat <adityarajsinghranawatjobs@gmail.com>
2026-01-01 14:03:42 +0530

    updated the comments structure and style

--- 16. Optional Properties/script.ts
+++ 16. Optional Properties/script.ts
@@ -11,10 +11,19 @@
 */
 
 /*
-  Now, when you create an object of type Person, you can choose whether or not to include the email property.
-  1 const alice: Person { name: "Alice", age: 30 };
-  2 const bob: Person = { name: "Bob", age: 25, email: "bob@example.com" };
-  In this example, the alice object does not include the email property, while the bob object does. Because the email property is marked as optional in the Person type definition, it is valid to create objects with or without it.
+  Now, when you create an object of type Person, 
+  you can choose whether or not to include 
+  the email property.
+
+  const alice: Person { name: "Alice", age: 30 };
+  const bob: Person = { name: "Bob", age: 25, email: "bob@example.com" };
+
+  In this example, the alice object does not include the 
+  email property, while the bob object does. 
+
+  Because the email property is marked as optional in the 
+  Person type definition, it is valid to create objects 
+  with or without it.
 */
 
 type Person = {
