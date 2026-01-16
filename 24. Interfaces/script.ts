143d981975101de7631796d16fd36fe54e62f514
Aditya Raj Singh Ranawat <adityarajsinghranawatjobs@gmail.com>
2026-01-16 21:15:13 +0530

    add interface documentation explaining its purpose and usage in TypeScript

---
+++ 24. Interfaces/script.ts
@@ -0,0 +1,14 @@
+// Interface:
+/*
+  Interface is a way to define a contract for the shape of an object. 
+  It specifies the properties and their types that an object must have. 
+  Interfaces are a powerful tool for enforcing a certain structure in your code.
+*/
+
+/*
+  While interfaces are commonly used to define the structure of objects, 
+  they are not limited to just objects. 
+  
+  Interfaces in TypeScript can also be used to describe the shape 
+  of functions and classes.
+*/
