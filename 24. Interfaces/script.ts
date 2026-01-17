9cf57fb1f4d356a3f9dd51082b1dfa4810dac60c
Aditya Raj Singh Ranawat <adityarajsinghranawatjobs@gmail.com>
2026-01-17 23:07:04 +0530

    add interfaces for object, function, and class with usage examples

--- 24. Interfaces/script.ts
+++ 24. Interfaces/script.ts
@@ -12,3 +12,52 @@
   Interfaces in TypeScript can also be used to describe the shape 
   of functions and classes.
 */
+
+// Interface definition for object
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+
+// Usage
+const examplePerson: Person = {
+  firstName: "Arjun",
+  lastName: "Kumar",
+  age: 30,
+};
+
+// Interface for function
+interface MathOperation {
+  (x: number, y: number): number;
+}
+
+// Usage
+const add: MathOperation = (a, b) => a + b;
+const subtract: MathOperation = (a, b) => a - b;
+
+console.log(add(5, 6));
+console.log(subtract(10, 6));
+
+// Interface for class
+interface Vehicle {
+  start(): void;
+  stop(): void;
+}
+
+// Class implementing the interface
+class Car implements Vehicle {
+  start() {
+    console.log("car started");
+  }
+
+  stop() {
+    console.log("car stopped");
+  }
+}
+
+// Usage
+const myCar = new Car();
+
+myCar.start();
+myCar.stop();
